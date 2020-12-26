1ca1d508374cf9c82c7c000b5a92004bfc52aa16
christine-cad <ccadman@student.unimelb.edu.au>
2020-12-26 18:28:33 +1100

    Fixing nav bar

--- all.js
+++ all.js
@@ -1,20 +1,19 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
- var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("myTopnav").style.top = "0";
-  } else {
-    document.getElementById("myTopnav").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// Menu-toggle button
 
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+        $("nav ul").toggleClass("showing");
+  });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+  if($(window).scrollTop()) {
+        $('nav').addClass('black');
+  }
+
+  else {
+        $('nav').removeClass('black');
+  }
+})
