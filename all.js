db886a8c73417eb8130fdd579e338adb05a545c8
christine-cad <ccadman@student.unimelb.edu.au>
2021-01-07 13:59:56 +1100

    progress bar

--- all.js
+++ all.js
@@ -42,3 +42,13 @@ $(document).ready(function(){
         } // End if
       });
     });
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
